bdd9d327886790351a4dc2ecf1f046acaf78e333
Kristine <magic.rooter@gmail.com>
2019-06-19 13:25:44 +0300

    Выполнила 7 задание

--- js/main.js
+++ js/main.js
@@ -63,3 +63,107 @@ for (var element = 0; element < photos.length; element++) {
 }
 
 pictureElement.appendChild(fragment);
+
+
+// Загрузка и обработка фото
+
+var ESC_KEYCODE = 27;
+var uploadFile = document.querySelector('#upload-file');
+var imageUpload = document.querySelector('.img-upload__overlay');
+var CloseButtonImageUploadHandler = imageUpload.querySelector('.img-upload__cancel');
+var imagePreview = imageUpload.querySelector('.img-upload__preview img');
+var photosEffectsList = imageUpload.querySelectorAll('.effects__item');
+var effectLevel = imageUpload.querySelector('.effect-level');
+
+var filters = [
+  'effects__preview--none',
+  'effects__preview--chrome',
+  'effects__preview--sepia',
+  'effects__preview--marvin',
+  'effects__preview--phobos',
+  'effects__preview--heat'
+];
+
+
+uploadFile.addEventListener('change', function () {
+  imageUpload.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscClose);
+});
+
+
+var closeImageUpload = function () {
+  imageUpload.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscClose);
+};
+
+CloseButtonImageUploadHandler.addEventListener('click', closeImageUpload);
+
+
+var onPopupEscClose = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeImageUpload();
+  }
+};
+
+var elementScaleSmaller = document.querySelector('.scale__control--smaller');
+var elementScaleBigger = document.querySelector('.scale__control--bigger');
+var elementScaleValue = document.querySelector('.scale__control--value');
+var minScale = 25;
+var maxScale = 100;
+var scaleStep = 25;
+var defaultFilterValue = 100;
+var currentScaleValue = defaultFilterValue;
+var elementImagePreviewWrap = document.querySelector('.img-upload__preview');
+
+var imageZoomOutHandler = function () {
+  currentScaleValue -= scaleStep;
+  setImagePreviewScale(currentScaleValue);
+};
+
+var imageZoomInHandler = function () {
+  currentScaleValue += scaleStep;
+  setImagePreviewScale(currentScaleValue);
+};
+
+
+elementScaleSmaller.addEventListener('click', function () {
+  if (currentScaleValue > minScale) {
+    imageZoomOutHandler();
+  }
+});
+elementScaleSmaller.addEventListener('keydown', function (evt) {
+  if (currentScaleValue > minScale) {
+    imageZoomOutHandler();
+  }
+});
+elementScaleBigger.addEventListener('click', function () {
+  if (currentScaleValue < maxScale) {
+    imageZoomInHandler();
+  }
+});
+elementScaleBigger.addEventListener('keydown', function (evt) {
+  if (currentScaleValue < minScale) {
+    imageZoomInHandler();
+  }
+});
+
+
+var setImagePreviewScale = function (value) {
+  elementScaleValue.value = value + '%';
+  elementImagePreviewWrap.style.transform = 'scale(' + value / 100 + ')';
+};
+
+var addFilterHandler = function (photo, filter) {
+  photo.addEventListener('click', function () {
+    imagePreview.className = filter;
+    if (filter === 'effects__preview--none') {
+      effectLevel.style.display = 'none';
+    } else {
+      effectLevel.style.display = 'block';
+    }
+  });
+};
+
+for (var x = 0; x < photosEffectsList.length; x++) {
+  addFilterHandler(photosEffectsList[x], filters[x]);
+}
