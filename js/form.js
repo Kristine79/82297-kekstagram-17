d010b96ac47a5ffb4b7a9bfafa9a7a302b770147
Kristine <magic.rooter@gmail.com>
2019-06-27 20:38:59 +0300

    Выполнила 10 задание

--- js/form.js
+++ js/form.js
@@ -4,12 +4,12 @@
 
 var ESC_KEYCODE = 27;
 var uploadFile = document.querySelector('#upload-file');
-var imageUpload = document.querySelector('.img-upload__overlay');
-var сloseButtonImageUpload = imageUpload.querySelector('.img-upload__cancel');
-var imagePreview = imageUpload.querySelector('.img-upload__preview img');
-var photosEffectsList = imageUpload.querySelectorAll('.effects__item');
-var imgUploadOverlayElement = document.querySelector('.img-upload__overlay');
-var effectLevel = imgUploadOverlayElement.querySelector('.effect-level');
+var imageUploadOverlay = document.querySelector('.img-upload__overlay');
+// var imageUpload = document.querySelector('.img-upload__overlay');
+var сloseButtonImageUpload = imageUploadOverlay.querySelector('.img-upload__cancel');
+var imagePreview = imageUploadOverlay.querySelector('.img-upload__preview img');
+var photosEffectsList = imageUploadOverlay.querySelectorAll('.effects__item');
+var effectLevel = imageUploadOverlay.querySelector('.effect-level');
 var elementScaleSmaller = document.querySelector('.scale__control--smaller');
 var elementScaleBigger = document.querySelector('.scale__control--bigger');
 var elementScaleValue = document.querySelector('.scale__control--value');
@@ -119,13 +119,13 @@ var pressEscButton = function (evt) {
 
 
 var openPopup = function () {
-  imageUpload.classList.remove('hidden');
+  imageUploadOverlay.classList.remove('hidden');
   document.addEventListener('keydown', pressEscButton);
   elementScaleValue.value = '100%';
 };
 
 var closePopup = function () {
-  imageUpload.classList.add('hidden');
+  imageUploadOverlay.classList.add('hidden');
   document.removeEventListener('keydown', pressEscButton);
 };
 
@@ -136,3 +136,104 @@ uploadFile.addEventListener('click', function () {
 сloseButtonImageUpload.addEventListener('click', function () {
   closePopup();
 });
+
+
+// Слайдер - ползунок и изменение интенсивности эффектов
+
+var effectLevelValue = effectLevel.querySelector('.effect-level__value');
+var effectLevelLine = effectLevel.querySelector('.effect-level__line');
+var effectLevelPin = effectLevel.querySelector('.effect-level__pin');
+var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
+var imgUploadPreview = imageUploadOverlay.querySelector('.img-upload__preview');
+var imageUploadPreview = imgUploadPreview.firstElementChild;
+
+var changeIntensity = function () {
+  effectLevelValue.value =
+    (
+      effectLevelPin.offsetLeft / effectLevelLine.clientWidth
+    ).toFixed(2) * 100;
+
+  if (imageUploadPreview.classList[0] === 'effects__preview--chrome') {
+    imageUploadPreview.style.filter =
+      'grayscale(' + effectLevelValue.value / 100 + ')';
+  } else if (
+    imageUploadPreview.classList[0] === 'effects__preview--sepia'
+  ) {
+    imageUploadPreview.style.filter =
+      'sepia(' + effectLevelValue.value / 100 + ')';
+  } else if (
+    imageUploadPreview.classList[0] === 'effects__preview--marvin'
+  ) {
+    imageUploadPreview.style.filter =
+      'invert(' + effectLevelValue.value + '%)';
+  } else if (
+    imageUploadPreview.classList[0] === 'effects__preview--phobos'
+  ) {
+    imageUploadPreview.style.filter =
+      'blur(' + (effectLevelValue.value / 100) * 3 + 'px)';
+  } else if (
+    imageUploadPreview.classList[0] === 'effects__preview--heat'
+  ) {
+    imageUploadPreview.style.filter =
+      'brightness(' + (effectLevelValue.value / 100) * 3 + ')';
+  }
+};
+
+
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoordsX = evt.clientX;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shiftX = startCoordsX - moveEvt.clientX;
+    startCoordsX = moveEvt.clientX;
+
+    var pinElementLeft = effectLevelPin.offsetLeft - shiftX;
+
+    var lineElementLeft = effectLevelLine.getBoundingClientRect().left;
+    var lineElementRight = effectLevelLine.getBoundingClientRect().right;
+    if (startCoordsX <= lineElementLeft) {
+      pinElementLeft = 0;
+    } else if (startCoordsX >= lineElementRight) {
+      pinElementLeft = effectLevelLine.clientWidth;
+    }
+
+    effectLevelPin.style.left = pinElementLeft + 'px';
+    effectLevelDepth.style.width = pinElementLeft + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mousemove', changeIntensity);
+
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+
+effectLevel.addEventListener('click', function (clickEvt) {
+  var coordsX = clickEvt.clientX;
+  var lineElementLeft = effectLevelLine.getBoundingClientRect().left;
+  var lineElementRight = effectLevelLine.getBoundingClientRect().right;
+
+  var elementLeft = coordsX - lineElementLeft;
+
+  if (coordsX <= lineElementLeft) {
+    elementLeft = 0;
+  } else if (coordsX >= lineElementRight) {
+    elementLeft = effectLevelLine.clientWidth;
+  }
+
+  effectLevelPin.style.left = elementLeft + 'px';
+  effectLevelDepth.style.width = elementLeft + 'px';
+});
+
+effectLevel.addEventListener('click', changeIntensity);
