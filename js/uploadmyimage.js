1e88e333111f2ae2d04a8a8ebbfd6f9652f53239
Kristine <magic.rooter@gmail.com>
2019-07-25 20:52:38 +0300

    Выполние последнее 23 задание

---
+++ js/uploadmyimage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['jpg', 'jpeg', 'gif', 'png'];
+  var imageChooser = document.querySelector('.img-upload__input');
+  var uploadPreview = document.querySelector('.img-upload__preview');
+  var imgPreview = uploadPreview.querySelector('img');
+
+  imageChooser.addEventListener('change', function () {
+    var file = imageChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        imgPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
